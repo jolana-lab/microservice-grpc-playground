3c1d2f8975a829a6727ec320cb33be0a62e02a68
Jijie LIU <liujijieseason@gmail.com>
2022-05-27 20:06:05 +0200

    feat: init gRPC microservice

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.TCP,
-    options: { retryAttempts: 5, retryDelay: 3000 },
+    transport: Transport.GRPC,
+    options: {
+      package: 'hero', // ['hero', 'hero2']
+      protoPath: join(__dirname, './hero/hero.proto'), // ['./hero/hero.proto', './hero/hero2.proto']
+    },
   });
 
   await app.startAllMicroservices();
