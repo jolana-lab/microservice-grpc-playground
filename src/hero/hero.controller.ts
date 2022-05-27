614cf3ffc51f2517c29c46f22b78dc6f270b247e
Jijie LIU <liujijieseason@gmail.com>
2022-05-27 20:33:55 +0200

    feat: create findOne function in hero service

--- src/hero/hero.controller.ts
+++ src/hero/hero.controller.ts
@@ -1,4 +1,16 @@
 import { Controller } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+import { Hero, HeroById } from 'src/interfaces';
 
 @Controller('hero')
-export class HeroController {}
+export class HeroController {
+  private readonly items = [
+    { id: 1, name: 'John' },
+    { id: 2, name: 'Doe' },
+  ];
+
+  @GrpcMethod('HeroesService')
+  findOne(data: HeroById): Hero {
+    return this.items.find(({ id }) => id === data.id);
+  }
+}
---
+++ src/interfaces/index.ts
@@ -0,0 +1,2 @@
+export * from './hero.interface';
+export * from './hero-by-id.interface';
