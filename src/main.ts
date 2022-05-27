b715f7c55cdace2fba8d729cdf13c2230d476729
Jijie LIU <liujijieseason@gmail.com>
2022-05-27 19:53:24 +0200

    feat: get the sum of numbers
    When client calls math get endpoint, microservice handles the sum work.

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,13 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.TCP,
-    },
-  );
-  await app.listen();
+  const app = await NestFactory.create(AppModule);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: { retryAttempts: 5, retryDelay: 3000 },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(3000);
 }
 bootstrap();
--- src/math/math.controller.ts
+++ src/math/math.controller.ts
@@ -1,8 +1,18 @@
-import { Controller } from '@nestjs/common';
-import { MessagePattern } from '@nestjs/microservices';
+import { Controller, Get, Inject } from '@nestjs/common';
+import { ClientProxy, MessagePattern } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
 
 @Controller('math')
 export class MathController {
+  constructor(@Inject('MATH_SERVICE') private client: ClientProxy) {}
+
+  @Get()
+  execute(): Observable<number> {
+    const pattern = { cmd: 'sum' };
+    const data = [1, 2, 3, 4, 5];
+    return this.client.send<number>(pattern, data);
+  }
+
   @MessagePattern({ cmd: 'sum' })
   sum(data: number[]): number {
     return (data || []).reduce((a, b) => a + b);
